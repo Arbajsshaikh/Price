00decafac7637d822fe7b83d5b9c03370f05a80c
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-02 17:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pandas as pd
 st.title("Excel Viewer App")
 
 # Upload Excel file
-uploaded_file = st.file_uploader("FINAL_REQUIREMENT.xlsx", type=["xlsx", "xls"])
+uploaded_file = st.file_uploader("JUNE.csv", type=["csv"])
 
 if uploaded_file is not None:
     # Read Excel file
-    df = pd.read_excel(uploaded_file, sheet_name="Sheet1")
+    df = pd.read_csv(uploaded_file)
 
     # Display DataFrame
     st.write("Excel File Contents:")
