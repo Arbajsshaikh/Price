9a2cfe72c355362c01433a262914fb260dec43fc
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-02 15:46:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load the data
-sorted_df = pd.read_excel(r"D:\data\SALIM_SIR\FINAL_REQUIREMENT.xlsx")
+sorted_df = pd.read_excel(r"FINAL_REQUIREMENT.xlsx")
 
 # Display the DataFrame
 st.dataframe(sorted_df)
